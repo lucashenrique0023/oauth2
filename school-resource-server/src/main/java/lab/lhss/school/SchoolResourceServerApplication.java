86211f7665db03314abc5506256c67268c1ce2f7
Lucas Henrique <lucas.henrique0023@gmail.com>
2022-03-05 14:58:23 -0300

    Resource Server First Commit

---
+++ school-resource-server/src/main/java/lab/lhss/school/SchoolResourceServerApplication.java
@@ -0,0 +1,13 @@
+package lab.lhss.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolResourceServerApplication.class, args);
+	}
+
+}
---
+++ school-resource-server/src/test/java/lab/lhss/school/SchoolResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.lhss.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
