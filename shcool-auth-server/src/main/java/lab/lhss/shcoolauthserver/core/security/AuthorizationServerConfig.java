535aa72739f1e0b920ad0a69abd0183972a494d5
Lucas Henrique <lucas.henrique0023@gmail.com>
2022-03-06 12:04:33 -0300

    Configuring Password Grant Type
    We have configured Password Grant Type Flow.
    
    How to test:
    
    1. Generate Access Token (Postman Request Example):
    curl --location --request POST 'localhost:8081/oauth/token' \
    --header 'Authorization: Basic c2Nob29sY2xpZW50OnNjaG9vbDEyMw==' \
    --header 'Content-Type: application/x-www-form-urlencoded' \
    --data-urlencode 'grant_type=password' \
    --data-urlencode 'username=lucasuser' \
    --data-urlencode 'password=123456'
    
    2. Copy Access Token and Make a new Request to Resource Server using it as Bearer Token:
    
    curl --location --request GET 'localhost:8080/student/1' \
    --header 'Authorization: Bearer 678b9134-67cc-4963-951b-35ea22737340'

---
+++ shcool-auth-server/src/main/java/lab/lhss/shcoolauthserver/core/security/AuthorizationServerConfig.java
@@ -0,0 +1,49 @@
+package lab.lhss.shcoolauthserver.core.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        // Enabling Client(s) to receive access tokens
+        clients.inMemory()
+                .withClient("schoolclient")
+                    .secret(passwordEncoder.encode("school123"))
+                    .authorizedGrantTypes("password")
+                    .scopes("write", "read")
+                    .accessTokenValiditySeconds(60 * 60 * 6)
+                    .and()
+                .withClient("checktoken")
+                    .secret(passwordEncoder.encode("check123"));
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) {
+        // isAuthenticated is a Spring Security Expression
+        // Only Authenticated can check tokens. (use permitAll() for not authenticated)
+        security.checkTokenAccess("isAuthenticated()");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        // Only for Password Flow, to analyze Resource Owner credentials
+        endpoints.authenticationManager(authenticationManager);
+    }
+}
