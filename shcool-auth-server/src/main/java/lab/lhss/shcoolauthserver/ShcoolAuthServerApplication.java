eadbc298b3fa2a8a98e7b418d247fee8db7094fa
Lucas Henrique <lucas.henrique0023@gmail.com>
2022-03-06 01:04:38 -0300

    Authorization Server First Commit

---
+++ shcool-auth-server/src/main/java/lab/lhss/shcoolauthserver/ShcoolAuthServerApplication.java
@@ -0,0 +1,13 @@
+package lab.lhss.shcoolauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShcoolAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShcoolAuthServerApplication.class, args);
+	}
+
+}
---
+++ shcool-auth-server/src/test/java/lab/lhss/shcoolauthserver/ShcoolAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.lhss.shcoolauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShcoolAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
