769335d55e88956415fba3dfe97fd26a2bdc693d
Lucas Henrique <lucas.henrique0023@gmail.com>
2022-03-21 22:18:06 -0300

    Get Operator Groups/Permissions and instantiate SimpleGrantedAuthority

--- shcool-auth-server/src/main/java/lab/lhss/shcoolauthserver/core/security/AuthUser.java
+++ shcool-auth-server/src/main/java/lab/lhss/shcoolauthserver/core/security/AuthUser.java
@@ -1,17 +1,18 @@
 package lab.lhss.shcoolauthserver.core.security;
 
 import lab.lhss.shcoolauthserver.domain.model.Operator;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
-import java.util.Collections;
+import java.util.Collection;
 
 public class AuthUser extends User {
 
     private String fullName;
     private Long userId;
 
-    public AuthUser(Operator operator) {
-        super(operator.getEmail(), operator.getPassword(), Collections.emptyList());
+    public AuthUser(Operator operator, Collection <? extends GrantedAuthority > authorities) {
+        super(operator.getEmail(), operator.getPassword(), authorities);
         this.userId = operator.getId();
         this.fullName = operator.getName();
     }
--- shcool-auth-server/src/main/java/lab/lhss/shcoolauthserver/core/security/JpaUserDetailsService.java
+++ shcool-auth-server/src/main/java/lab/lhss/shcoolauthserver/core/security/JpaUserDetailsService.java
@@ -3,10 +3,16 @@ package lab.lhss.shcoolauthserver.core.security;
 import lab.lhss.shcoolauthserver.domain.model.Operator;
 import lab.lhss.shcoolauthserver.domain.model.OperatorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 @Service
 public class JpaUserDetailsService implements UserDetailsService {
@@ -14,14 +20,22 @@ public class JpaUserDetailsService implements UserDetailsService {
     @Autowired
     OperatorRepository studentRepository;
 
+    @Transactional(readOnly = true)
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         Operator operator = studentRepository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("Email not found."));
 
-        return new AuthUser(operator);
+        return new AuthUser(operator, getAuthorities(operator));
 
+    }
+
+    private Collection<GrantedAuthority> getAuthorities(Operator operator) {
+        return operator.getGroups().stream()
+                .flatMap(group -> group.getPermissions().stream())
+                .map(permission -> new SimpleGrantedAuthority(permission.getName().toUpperCase()))
+                .collect(Collectors.toSet());
     }
 
 }
