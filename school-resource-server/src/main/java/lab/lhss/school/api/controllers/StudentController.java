ce2f2dd2ebbddb769f50ace673a94638ab5b813b
Lucas Henrique <lucas.henrique0023@gmail.com>
2022-03-23 22:27:52 -0300

    Using Authorities to limit access to resources with @PreAuthorize
    Another way to limit access to resources using @PreAuthorize annotation on controller methods.

--- school-resource-server/src/main/java/lab/lhss/school/api/controllers/StudentController.java
+++ school-resource-server/src/main/java/lab/lhss/school/api/controllers/StudentController.java
@@ -3,6 +3,7 @@ package lab.lhss.school.api.controllers;
 import lab.lhss.school.domain.model.Student;
 import lab.lhss.school.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -15,16 +16,19 @@ public class StudentController {
     StudentService studentService;
 
     @GetMapping("/{studentId}")
+    @PreAuthorize("hasAuthority('SEARCH_STUDENT')")
     public Student find(@PathVariable Long studentId) {
         return studentService.findById(studentId);
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('CREATE_STUDENT')")
     public Student save(@RequestBody @Valid Student student) {
         return studentService.save(student);
     }
 
     @DeleteMapping("/{walletId}")
+    @PreAuthorize("hasAuthority('DELETE_STUDENT')")
     public void delete(@PathVariable Long studentId) {
         studentService.deleteById(studentId);
     }
--- school-resource-server/src/main/java/lab/lhss/school/core/security/ResourceServerConfig.java
+++ school-resource-server/src/main/java/lab/lhss/school/core/security/ResourceServerConfig.java
@@ -2,6 +2,7 @@ package lab.lhss.school.core.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,32 +16,23 @@ import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/h2-console/**").permitAll()
-                .antMatchers(HttpMethod.POST, "/student/**").hasAuthority("EDIT_STUDENT")
-                .antMatchers(HttpMethod.GET, "/student/**").hasAuthority("SEARCH_STUDENT")
-                .antMatchers(HttpMethod.PUT, "/student/**").hasAuthority("CREATE_STUDENT")
-                .antMatchers(HttpMethod.DELETE, "/student/**").hasAuthority("DELETE_STUDENT")
-                .anyRequest().denyAll()
                 .and()
                 .cors()
                 .and()
                 .oauth2ResourceServer()
                     .jwt()
-                    .jwtAuthenticationConverter(jwtGrantedAuthoritiesConverter());
+                    .jwtAuthenticationConverter(jwtAuthenticationConverter());
 
-        http.csrf().ignoringAntMatchers("/h2-console/**");http.csrf().disable();
+        //http.csrf().ignoringAntMatchers("/h2-console/**");
+        http.csrf().disable();
         http.headers().frameOptions().sameOrigin();
-    }
-
-    private Converter jwtGrantedAuthoritiesConverter() {
-        var jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
-        jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("authorities");
-        return jwtGrantedAuthoritiesConverter;
     }
 
     // Necessary for extract Authorities from jwt array of authorities
